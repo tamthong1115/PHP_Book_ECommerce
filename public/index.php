cd02b827aae79b675c75b07fb91ac25141f32d9f
Tam Thong <powder@mail.com>
2024-07-19 23:16:31 +0700

    update skeleton

--- public/index.php
+++ public/index.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Store</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
 
 </head>
 
 <body>
-    
+
 </body>
 
 </html>
