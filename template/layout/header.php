059f426a831ac9e2d19fa13c46493a3444c7ce53
TamThong <thong@mail.com>
2024-07-21 12:40:01 +0100

    XAMPP compatible

--- template/layout/header.php
+++ template/layout/header.php
@@ -8,10 +8,6 @@
     <link rel="stylesheet" href="<?= asset('public/css/global/styles.css') ?>" type="text/css">
     <link rel="stylesheet" href="<?= asset('public/css/template/header.css')?>" type="text/css">
 
-    <!-- Fonts -->
-     <link rel="stylesheet" href="<?= asset('public/fonts/css/fontawesome.min.css')?>">
-     <link rel="stylesheet" href="<?= asset('public/fonts/css/brands.min.css')?>">
-     <link rel="stylesheet" href="<?= asset('public/fonts/css/solid.min.css')?>">
 </head>
 
 <body>
@@ -22,11 +18,9 @@
             <ul class="header__navbar">
                 <li><a class="active" href="index.php">Home</a></li>
                 <li><a href="#">Shop</a></li>
-                <li><a href="#">Blog</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
-                <li><a class="header__cart" href="#"><i class="fa-regular fa-cart-shopping"></i></a></li>
-            </ul>
+                <li><a class="header__cart" href="#"><img src="<?= asset('public/icons/shopping-cart.svg') ?>" alt="cart-logo"></a></li>            </ul>
         </div>
     </section>
 </body>
